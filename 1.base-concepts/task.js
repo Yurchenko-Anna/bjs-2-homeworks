d4e034843a3b1f631f26df1395d1389232316f51
Anna Yurchenko <sukhorukikh.anna@gmail.com>
2022-11-29 14:01:56 +0300

    The first task is ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,21 @@
 "use strict"
+solveEquation(1, 2, 1);
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let discriminant = b**2 - 4*a*c;
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr.push((-b)/(2*a));
+  } else {
+    arr.push((-b + Math.sqrt(discriminant))/(2*a));
+    arr.push((-b - Math.sqrt(discriminant))/(2*a));
+  }
+  console.log(arr);
+  return arr;  
 }
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
