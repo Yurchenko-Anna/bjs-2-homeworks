fc41a41bb99b34ee28e135bc59b63cdb9415d6c8
Anna Yurchenko <sukhorukikh.anna@gmail.com>
2022-12-06 15:27:42 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,30 @@
+compareArrays([8, 1, 2], [8, 1, 2])
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length){
+    console.log(arr1.every((value, index) => value === arr2[index]));
+  }  else {
+    console.log('false');
+  }
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+const people = [
+    {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+    {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+    {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+    {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+    {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+    {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+    {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+    {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+    {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+    {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+    {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+    {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+    {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+    {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+  ]
+  getUsersNamesInAgeRange(people, "мужской");
+  function getUsersNamesInAgeRange(users, gender) {
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {acc+=item; if (index === arr.length - 1){return acc / arr.length;} return acc}, 0);
+    console.log(result)
+  }
