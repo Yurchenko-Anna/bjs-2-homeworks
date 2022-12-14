5463f2637720cac0b36311af059fb9c442ae3637
Anna Yurchenko <sukhorukikh.anna@gmail.com>
2022-12-14 23:45:01 +0300

    Четвертое задание для проверки

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = []; 
 }
-
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if ("marks" in this) {
+    this.marks = [...marks];
+  }  
 }
 
 Student.prototype.getAverage = function () {
-  
+   if ("marks" in Student === false || marks.length === 0) {
+    return 0;
+  } else {
+     let average = Student.marks.reduce((acc, item, index, arr) => {acc+=item; if (index === arr.length - 1){return acc / arr.length;} return acc}, 0) 
+     return average;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete student.subjec;
+  delete student.marks;
+  student.exclude = reason;
 }
