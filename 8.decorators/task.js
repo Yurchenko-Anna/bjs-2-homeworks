da7084675b4ce2057d3c189d0da4ac693a0b80d4
Anna Yurchenko <sukhorukikh.anna@gmail.com>
2022-12-28 14:37:02 +0300

    Восьмое задание для проверки

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,38 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  return function(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((objectInCache) => objectInCache.hash === hash);
+    if (objectInCache) {
+      console.log('Из кэша: ' + objectInCache.value);
+      return 'Из кэша: ' + objectInCache.value;
+    }
+    let value = func(...args);
+    cache.push({ hash, value });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log('Вычисляем: ' + value);
+    return 'Вычисляем: ' + value;
+  }
 }
 
-//Задача № 2
+
+
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+   function wrapper(...args){
+    wrapper.allcount += 1;
+    if (timeoutId){
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        timeoutId = null;
+        console.log(func(...args)); 
+        wrapper.count +=1;
+    }, delay);
+  }
+  wrapper.count = 0;
+  wrapper.allcount = 0;
+  return wrapper;
 }
